4c450d8822ac79fdef2041602900bf161c0fc84a
Jeremiah Agbama <agbamajeremiah@gmail.com>
2022-05-10 09:09:47 +0100

    Add SafeArea to WebView

--- lib/view/web.dart
+++ lib/view/web.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:okra_widget/models/okra_handler.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -24,43 +25,59 @@ class _WebState extends State<Web> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      WebView(
-          initialUrl: "https://mobile.okra.ng/",
-          onPageFinished: (response) {
-            setState(() {isLoading = false;});
-            String jsonOptions = json.encode(widget.okraOptions);
-            _controller.evaluateJavascript("openOkraWidget('$jsonOptions')");
-          },
-          javascriptMode: JavascriptMode.unrestricted,
-          gestureRecognizers: [
-            Factory(() => VerticalDragGestureRecognizer()),
-            Factory(() => TapGestureRecognizer()),
-          ].toSet(),
-          javascriptChannels: Set.from([
-            JavascriptChannel(
-                name: 'FlutterOnSuccess',
-                onMessageReceived: (JavascriptMessage message) {
-                  okraHandler =
-                      new OkraHandler(true, true, false, false, message.message);
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle(
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      child: Scaffold(
+        body: SafeArea(
+          child: Stack(children: [
+            WebView(
+                initialUrl: "https://mobile.okra.ng/",
+                onPageFinished: (response) {
+                  setState(() {
+                    isLoading = false;
+                  });
+                  String jsonOptions = json.encode(widget.okraOptions);
+                  _controller
+                      .evaluateJavascript("openOkraWidget('$jsonOptions')");
+                },
+                javascriptMode: JavascriptMode.unrestricted,
+                gestureRecognizers: [
+                  Factory(() => VerticalDragGestureRecognizer()),
+                  Factory(() => TapGestureRecognizer()),
+                ].toSet(),
+                javascriptChannels: Set.from([
+                  JavascriptChannel(
+                      name: 'FlutterOnSuccess',
+                      onMessageReceived: (JavascriptMessage message) {
+                        okraHandler = new OkraHandler(
+                            true, true, false, false, message.message);
+                      }),
+                  JavascriptChannel(
+                      name: 'FlutterOnError',
+                      onMessageReceived: (JavascriptMessage message) {
+                        okraHandler = new OkraHandler(
+                            true, false, true, false, message.message);
+                      }),
+                  JavascriptChannel(
+                      name: 'FlutterOnClose',
+                      onMessageReceived: (JavascriptMessage message) {
+                        Navigator.pop(context, okraHandler);
+                      })
+                ]),
+                onWebViewCreated: (webViewController) {
+                  _controller = webViewController;
                 }),
-            JavascriptChannel(
-                name: 'FlutterOnError',
-                onMessageReceived: (JavascriptMessage message) {
-                  okraHandler =
-                      new OkraHandler(true, false, true, false,  message.message);
-                }),
-            JavascriptChannel(
-                name: 'FlutterOnClose',
-                onMessageReceived: (JavascriptMessage message) {
-                  Navigator.pop(context, okraHandler);
-                })
+            isLoading
+                ? Center(
+                    child: CircularProgressIndicator(),
+                  )
+                : Container(width: 0, height: 0, color: Colors.transparent),
           ]),
-          onWebViewCreated: (webViewController) {
-            _controller = webViewController;
-          }),
-      isLoading ? Center( child: CircularProgressIndicator(),)
-          : Container(width: 0, height: 0,  color: Colors.transparent),
-    ]);
+        ),
+      ),
+    );
   }
 }
